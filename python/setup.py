793eb8019d4332d659c3ef7fe25252fb9d2b8d8e
Cameron Fairchild <cameron@opentensor.ai>
2022-10-19 15:13:01 -0400

    add test file and setup

---
+++ python/setup.py
@@ -0,0 +1,87 @@
+# The MIT License (MIT)
+# Copyright © 2022 Opentensor Foundation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+import os
+import re
+import sys
+from io import open
+from os import path
+from typing import Optional
+
+from pkg_resources import parse_requirements
+from setuptools import setup, find_packages
+
+package_data = {
+    'bittensor': []
+}
+
+platform: Optional[str] = os.environ.get('BT_BUILD_TARGET') or sys.platform
+
+# Check platform and remove unsupported subtensor node api binaries.
+if platform == "linux" or platform == "linux2":
+    # linux
+    package_data['bittensor'].append('subtensor-node-api-linux')
+elif platform == "darwin":
+    # OS X
+    package_data['bittensor'].append('subtensor-node-api-macos')
+else: # e.g. platform == None
+    # neither linux or macos
+    # include neither binaries
+    pass
+
+setup(
+    name='bittensor',
+    version=version_string,
+    description='bittensor',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/opentensor/bittensor',
+    author='bittensor.com',
+    packages=find_packages(),
+    include_package_data=True,
+    author_email='',
+    license='MIT',
+    install_requires=install_requires,
+    scripts=['bin/btcli', 'bin/subtensor-node-api-linux'],
+    #package_data=package_data,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=[
+        'nlp',
+        'crypto',
+        'machine learning',
+        'ml',
+        'tao'
+    ],
+    python_requires='>=3.7'
+)
---
+++ run_test.sh
@@ -0,0 +1,1 @@
+python3 -m subtensornodeapi sync_and_save -f "./test.json"
